55ba4e8db6d34e8b2c331664e93ca936ac989dcd
zbl14 <ifthereisoneday@gmail.com>
2022-08-12 13:52:31 -0700

    fix identation

--- CretaceousPark/Model/CretaceousParkContext.cs
+++ CretaceousPark/Model/CretaceousParkContext.cs
@@ -2,13 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CretaceousPark.Models
 {
-    public class CretaceousParkContext : DbContext
+  public class CretaceousParkContext : DbContext
+  {
+    public CretaceousParkContext(DbContextOptions<CretaceousParkContext> options)
+        : base(options)
     {
-        public CretaceousParkContext(DbContextOptions<CretaceousParkContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Animal> Animals { get; set; }
     }
+
+    public DbSet<Animal> Animals { get; set; }
+  }
 }
